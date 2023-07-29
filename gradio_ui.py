b0efb38870d48dbcbc1964914978d287c505e7a2
RajKKapadia <raajforyou@gmail.com>
2023-07-29 21:21:24 +0530

    ran during recording

--- gradio_ui.py
+++ gradio_ui.py
@@ -5,5 +5,9 @@ def greet(text: str) -> str:
     return text
 
 
-demo = gr.Interface(fn=greet, inputs=gr.components.Textbox(
-    label='Input'), outputs=gr.components.Textbox(label='Output'), allow_flagging='never')
+demo = gr.Interface(
+    fn=greet,
+    inputs=gr.components.Textbox(label='Input'),
+    outputs=gr.components.Textbox(label='Output'),
+    allow_flagging='never'
+)
